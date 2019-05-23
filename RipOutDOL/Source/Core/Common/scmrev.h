4e8e42b30a63d3bba9bc49d6535a60376ba9242f
Darren Hoehna <dhoehna@yahoo.com>
2019-05-22 22:09:24 -0700

    Addounf Common

---
+++ RipOutDOL/Source/Core/Common/scmrev.h
@@ -0,0 +1,6 @@
+#define SCM_REV_STR "e10a472134fdf557ec35505eb5fb7c74ba9f2af7"
+#define SCM_DESC_STR "5.0-9926-dirty"
+#define SCM_BRANCH_STR "master"
+#define SCM_IS_MASTER 1
+#define SCM_DISTRIBUTOR_STR "None"
+#define SCM_UPDATE_TRACK_STR ""
